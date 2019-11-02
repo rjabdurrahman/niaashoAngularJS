89530ce12f67892f70c51f218335da284c4c4e94
rjabdurrahman <md.rahman0296@gmail.com>
2019-11-02 15:48:25 +0600

    Added: Htaccess for https

---
+++ app.js
@@ -0,0 +1,40 @@
+var app = angular.module('myApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+  $routeProvider
+
+  .when('/', {
+    templateUrl : 'catpages/product.html',
+    controller  : 'HomeCntlr'
+  })
+  .when('/electronics', {
+    templateUrl : 'catpages/product.html',
+    controller  : 'ElectronicsCntlr'
+  })
+
+  .otherwise({redirectTo: '/'});
+});
+app.controller('HomeCntlr', function($scope){
+    $scope.cat = 'Main';
+    $scope.products = [
+        {
+            name: "Main 1",
+            price: 45
+        },
+        {
+            name: "Main 2",
+            price: 80
+        },
+        {
+            name: "Main 3",
+            price: 90
+        },
+        {
+            name: "Main 4",
+            price: 20
+        }
+    ];
+});
+app.run(function($rootScope) {
+  $rootScope.cat = 'Main';
+})
---
+++ main.js
@@ -0,0 +1,56 @@
+function $js(id) {
+    return document.getElementById(id);
+}
+//Category Navigation
+jQuery(document).ready(function () {
+    $(document).ready(function(){
+        $("#catBtn").click(function(){
+            $("#catUl").slideToggle("slow");
+        });
+    });
+    var subMenu = jQuery("ul#catUl li ul li");
+    var linkClick = jQuery("ul#catUl li").filter(":has(ul)");
+
+    subMenu.hide();
+
+    linkClick.click(function () {
+        $(this).find('ul li').slideToggle("fast, 100");
+    });
+});
+//Slider Start
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+setInterval(function () { showDivs(++slideIndex) }, 3000);
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-white", "");
+    }
+    x[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " w3-white";
+}
+//Category Navigation
+// $js("catBtn").addEventListener('click', function () {
+//     var cList = $js("catUl").className.split(' ');
+//     if (cList.indexOf("cat-nav-show") == -1) {
+//         $js("catUl").classList.add("class", "cat-nav-show");
+//     }
+//     else {
+//         $js("catUl").classList.remove("class", "cat-nav-show");
+//     }
+// });
